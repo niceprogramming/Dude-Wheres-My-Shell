4509d2807fc3c954afe02ab9da9c4e80c78b77cf
niceprogramming <Test@test.com>
2019-01-26 11:22:43 -0600

    Scripts

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+    [SerializeField]
+    private float rotationSpeed = 50;
+
+
+    void Update()
+    {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+
+        transform.Translate(0, 0, translation);
+
+
+        transform.Rotate(0, rotation, 0);
+    }
+}
