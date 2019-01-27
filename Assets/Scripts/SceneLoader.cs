6252d39c07b9db6d5d9d93a42681cc57ae4ad4dd
niceprogramming <Test@test.com>
2019-01-26 19:58:03 -0600

    Scenes and animations

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Scene currentScene;
+    void Start()
+    {
+       currentScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScene.buildIndex + 1);
+
+    }
+}
