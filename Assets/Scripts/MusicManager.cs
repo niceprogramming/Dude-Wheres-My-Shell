93ec4e60dc92d3b0e8e12dfd6de971039d2bbc58
niceprogramming <Test@test.com>
2019-01-27 11:56:20 -0600

    Music management

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource source;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (source.isPlaying) return;
+        source.Play();
+    }
+
+    public void StopMusic()
+    {
+        source.Stop();
+    }
+}
