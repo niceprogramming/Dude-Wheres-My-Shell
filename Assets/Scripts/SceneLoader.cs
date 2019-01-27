a7171147d6f79c2add1661047cb1434c811f3c92
niceprogramming <Test@test.com>
2019-01-27 15:14:59 -0600

    Hopefully final commit

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -15,6 +16,7 @@ public class SceneLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(currentScene.buildIndex);
